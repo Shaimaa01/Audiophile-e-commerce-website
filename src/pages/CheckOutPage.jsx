684781e9eab345a246cd0f29927e68d9eb04ee60
shaimaa <shisko939@gmail.com>
2025-01-30 11:01:51 +0200

    update validationSchema

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -17,7 +17,11 @@ function CheckOutPage() {
   };
 
   const validationSchema = Yup.object({
-    name: Yup.string().required("Full name is required"),
+    name: Yup.string()
+      .min(3, "Name must be at least 3 characters")
+      .max(50, "Name can't be longer than 50 characters")
+      .matches(/^[a-zA-Z\s]+$/, "Name should only contain letters and spaces")
+      .required("Full name is required"),
     email: Yup.string()
       .email("Invalid email address")
       .required("Email is required"),
@@ -27,15 +31,34 @@ function CheckOutPage() {
         "Phone number is not valid"
       )
       .required("Phone number is required"),
-    address: Yup.string().required("Address is required"),
-    zip: Yup.string().required("ZIP code is required"),
-    city: Yup.string().required("City is required"),
-    country: Yup.string().required("Country is required"),
+    address: Yup.string()
+      .min(5, "Address is too short")
+      .max(100, "Address is too long")
+      .matches(/^[A-Za-z0-9\s,.-]+$/, "Address contains invalid characters")
+      .required("Address is required"),
+    zip: Yup.string()
+      .matches(/^[A-Z0-9]{1,5}\s?-?\s?[A-Z0-9]{3,6}$/i, "ZIP is not valid")
+      .required("ZIP code is required"),
+    city: Yup.string()
+      .min(2, "City name is too short")
+      .matches(/^[a-zA-Z\s]+$/, "City should only contain letters")
+      .required("City is required"),
+    country: Yup.string()
+      .min(2, "Country name is too short")
+      .max(20, "Country name is too long")
+      .matches(/^[a-zA-Z\s'-.]+$/, "Country name contains invalid characters")
+      .required("Country is required"),
     paymentMethod: Yup.string().required("PaymentMethod is required"),
     eMoneyNumber: Yup.string()
-      .required("e-Money number is required ")
-      .nullable(),
-    eMoneyPIN: Yup.string().required("e-Money PIN is required ").nullable(),
+      .matches(
+        /^[a-zA-Z0-9]+$/,
+        "e-Money number should contain only letters and numbers"
+      )
+      .required("e-Money number is required "),
+    eMoneyPIN: Yup.string()
+      .length(4, "e-Money PIN must be exactly 4 digits")
+      .matches(/^\d{4}$/, "e-Money PIN must be numeric")
+      .required("e-Money PIN is required "),
   });
 
   const handleSubmit = (values, { resetForm }) => {
